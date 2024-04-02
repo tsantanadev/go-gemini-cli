1396c9c608f2375e2f251fc439a10dd0fc8e1e44
Thiago <thiago91098@gmail.com>
2024-04-01 23:18:11 -0300

    Add config command to set Gemini API Key

---
+++ cmd/config.go
@@ -0,0 +1,40 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var (
+	apiKey string
+)
+
+// configCmd represents the config command
+var configCmd = &cobra.Command{
+	Use:   "config",
+	Short: "Config command is used to configure the Gemini API Key",
+	Long:  ``,
+	Run: func(cmd *cobra.Command, args []string) {
+		if apiKey != "" {
+			viper.Set("gemini.api-key", apiKey)
+
+			if err := viper.WriteConfig(); err != nil {
+				fmt.Println("Error writing config file:", err)
+			}
+
+		} else {
+			fmt.Println("No API Key provided")
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(configCmd)
+
+	configCmd.Flags().StringVarP(&apiKey, "api-key", "k", "", "The API key to use for Gemini")
+}
